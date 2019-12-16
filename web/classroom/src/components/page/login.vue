27d7d2ba2a0217391f2cf835e538df8c09679d54
spvycf <545997765@qq.com>
2019-12-16 16:08:52 +0800

    add

---
+++ web/classroom/src/components/page/login.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="pos">
+    登录吧
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'login'
+  }
+</script>
+<style scoped>
+
+</style>
--- web/classroom/src/main.js
+++ web/classroom/src/main.js
@@ -1,21 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-import App from './App'
+import login from './components/page/login'
 import router from './router'
-//import store from './store/'
-
 
 Vue.config.productionTip = false
-Vue.use(ElementUI);
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  store,
-  components: { App },
-  template: '<App/>'
+  components: { login },
+  template: '<login/>'
 })
